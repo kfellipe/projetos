fce394f7ff36ad9bd141769c82a4a7a1f4f0afe1
Kauã Fellipe <kauazeta87@gmail.com>
2021-05-10 20:34:25 -0300

    adding jogo

---
+++ Jogo01/jogo.py
@@ -0,0 +1,48 @@
+import random
+def lin():
+    print("=" * 40)
+while True:
+    escuser1 = input("Escolha um número: ")
+    lin()
+    if escuser1.isnumeric():
+        escuser = int(escuser1)
+        escom = random.randint(0, escuser)
+        tent = 0
+        while True:
+            tryuser1 = input(f"tente acertar o número de 0 a {escuser}: ")
+            tent += 1
+            if tryuser1.isnumeric():
+                tryuser = int(tryuser1)
+                if tryuser < escom:
+                    print("Mais!!!")
+                    lin()
+                if tryuser > escom:
+                    print("Menos!!!")
+                    lin()
+                if tryuser == escom:
+                    print(f"PARABÈNS!!! \nVocê acertou o número! \nVocê fez em {tent} tentativas!!!")
+                    lin()
+                    print(f"Deseja jogar novamente? \n1 - Tentar Novamente \n2 - Sair")
+                    while True:
+                        esctent1 = input("Escolha: ")
+                        if esctent1.isnumeric():
+                            esctent = int(esctent1)
+                            if esctent == 1:
+                                lin()
+                                break
+                            if esctent == 2:
+                                print("Obrigado por Jogar!")
+                                exit(0)
+                            else:
+                                print("[ERROR]Digite 1 ou 2!")
+                                lin()
+                        else:
+                            print("[ERROR] Digite apenas números! ")
+                            lin()
+                    break
+            else:
+                print("[ERROR] Digite apenas números! ")
+                lin()
+    else:
+        print("[ERROR] Digite apenas números! ")
+        lin()
