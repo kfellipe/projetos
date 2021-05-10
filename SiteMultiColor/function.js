2d9a37a16cf2b724a00516acc1cfd7ae3100a2c8
Kauã Fellipe <kauazeta87@gmail.com>
2021-05-10 15:42:17 -0300

    Adding SiteMultiColor

---
+++ SiteMultiColor/function.js
@@ -0,0 +1,18 @@
+let dom = window.document.body.style
+let char = window.document.getElementById("Titulo")
+function azul(){
+    dom.backgroundColor="blue"
+    char.style.color="black"
+}
+function verm(){
+    dom.backgroundColor="red"
+    char.style.color="black"
+}
+function verde(){
+    dom.backgroundColor="green"
+    char.style.color="black"
+}
+function preto(){
+    dom.backgroundColor="black"
+    char.style.color="white"
+}
